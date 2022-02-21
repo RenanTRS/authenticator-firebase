3234b45efa0fbee05486d11ac937c0f47ead4c34
Renan Souza <renantrsouza@gmail.com>
2022-02-21 12:27:16 -0400

    Add reset css

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import { Reset } from './style/reset'
+import { Base } from './style/base'
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Reset />
+    <Base />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
