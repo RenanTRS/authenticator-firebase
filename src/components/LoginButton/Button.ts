9f2b72d7bff6993f09c9a21d5ef5811b8cd13c4c
Renan Souza <renantrsouza@gmail.com>
2022-02-21 16:48:38 -0400

    Add improvments

---
+++ src/components/LoginButton/Button.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+    width: 13.5rem;
+    height: 13.5rem;
+
+    border: none;
+    border-radius: 50%;
+
+    cursor: pointer;
+
+    img{
+        pointer-events: none;
+    }
+`
