001d417622d5f63894666e7ff86caba0dc11827a
Renan Souza <renantrsouza@gmail.com>
2022-02-21 14:32:01 -0400

    create style for main element

---
+++ src/components/Main/index.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Main = styled.main`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    min-height: 100vh;
+
+    padding: 1.5rem;
+`
