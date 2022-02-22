cd637ea4134534f606070d46e24e90478033099b
Renan Souza <renantrsouza@gmail.com>
2022-02-22 12:57:18 -0400

    authentication with firebase auth

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -2,14 +2,19 @@
 import { Google } from './Google'
 import googleImg from '../../assets/image/googleImg.svg'
 import { LoginContainer } from '../LoginContainer'
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import {auth} from '../../services/firebase';
 
 export const LoginButton = () => {
-    const handleClick = () => {
-        console.log('click')
+    const provider = new  GoogleAuthProvider();
+    const register = async () => {
+        const user = await signInWithPopup(auth, provider);
+        console.log(user)
     }
+
     return(
         <LoginContainer>
-            <Google onClick={handleClick}><img src={googleImg} alt="Google" /></Google>
+            <Google onClick={register}><img src={googleImg} alt="Google" /></Google>
         </LoginContainer>
     )
 }
