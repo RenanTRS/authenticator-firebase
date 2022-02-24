4528f7cdc38d5ec83e8f6ce1c6abfb6c20fde2f8
Renan Souza <renantrsouza@gmail.com>
2022-02-24 15:02:26 -0400

    create navigate

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -5,19 +5,23 @@ import googleImg from '../../assets/image/googleImg.svg'
 import githubImg from '../../assets/image/githubImg.svg'
 import { LoginContainer } from '../LoginContainer'
 import {useAuth} from '../../hooks/useAuth'
+import {useNavigate} from 'react-router-dom'
 
 export const LoginButton = () => {
     const {user, signInWithGoogle, signInWithGitHub} = useAuth();
+    const navigate = useNavigate()
     
     const loginGoogle = async () => {
         if(!user){
             await signInWithGoogle()
         }
+        navigate('/room')
     }
     const loginGitHub = async () => {
         if(!user){
             await signInWithGitHub()
         }
+        navigate('/room')
     }
 
 
